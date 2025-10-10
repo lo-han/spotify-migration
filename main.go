81bc49b42506b3d3e0e874dcef8c1464df237d9f
Lo Han <lo.sa@dock.tech>
2025-10-10 20:18:33 -0300

    Feat: Added spotify authentication

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"spotify_migration/adapters/extractor"
@@ -8,6 +9,9 @@ import (
 	"spotify_migration/domain"
 	"spotify_migration/ports"
 	"spotify_migration/usecases"
+
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+	"golang.org/x/oauth2"
 )
 
 func main() {
@@ -18,23 +22,40 @@ func main() {
 	resourceKind := os.Args[1]
 	resourceName := os.Args[2]
 
+	ctx := context.Background()
+
 	var spotify ports.IExtractor
+
+	auth, token := spotifyAuth(ctx)
 
 	youtube := importer.NewYoutubeImporter()
 
 	switch resourceKind {
 	case domain.PlaylistKind:
-		spotify = extractor.NewSpotifyPlaylistExtractor()
+		spotify = extractor.NewSpotifyPlaylistExtractor(ctx, auth, token)
 
 	case domain.AlbumKind:
-		spotify = extractor.NewSpotifyAlbumExtractor()
+		spotify = extractor.NewSpotifyAlbumExtractor(ctx, auth, token)
 	}
 
 	migration := usecases.NewMigration(spotify, youtube)
 
-	if ok, err := migration.Migrate(resourceName); err != nil {
+	if ok, err := migration.Migrate(ctx, resourceName); err != nil {
 		fmt.Println("Error migrating resource:", err)
 	} else if ok {
 		fmt.Printf("%s migrated successfully: %s\n", resourceKind, resourceName)
 	}
+}
+
+func spotifyAuth(ctx context.Context) (*spotifyauth.Authenticator, *oauth2.Token) {
+	auth := spotifyauth.New(
+		spotifyauth.WithScopes(spotifyauth.ScopeUserReadPrivate),
+	)
+
+	token, err := auth.Exchange(ctx, os.Getenv("CODE"))
+	if err != nil {
+		return nil, nil
+	}
+
+	return auth, token
 }
