aec122888a265712d996e619fca1bfa9af7b2f4b
Lo Han <lo.sa@dock.tech>
2025-10-10 21:21:27 -0300

    WIP: Implementing inser strategies

---
+++ adapters/importing_strategy/just_insert_strategy.go
@@ -0,0 +1,46 @@
+package importing_strategy
+
+import (
+	"context"
+	"spotify_migration/ports"
+
+	"google.golang.org/api/youtube/v3"
+)
+
+func NewYoutubeJustInsert(service *youtube.Service) ports.IImportingStrategy {
+	return &youtubeJustInsert{service: service}
+}
+
+type youtubeJustInsert struct {
+	service *youtube.Service
+}
+
+func (u *youtubeJustInsert) UpdateItems(ctx context.Context, resourceName string, collectionID string, itemIDs []string) error {
+	err := u.insertAll(ctx, collectionID, itemIDs)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u *youtubeJustInsert) insertAll(ctx context.Context, collectionID string, itemIDs []string) error {
+	if collectionID == "" || len(itemIDs) == 0 {
+		return nil
+	}
+	for _, itemID := range itemIDs {
+		err := u.addItemToPlaylist(ctx, collectionID, itemID)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (u *youtubeJustInsert) addItemToPlaylist(ctx context.Context, collectionID string, itemID string) error {
+	// Simulate adding an item to the playlist
+	if collectionID == "" || itemID == "" {
+		return nil
+	}
+	return nil
+}
